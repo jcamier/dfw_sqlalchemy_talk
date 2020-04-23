6b285c9c8afe81117486255bc2106f8c7293d3c4
Jacques Camier <jcamier@gmail.com>
2020-04-23 18:52:58 -0500

    add sa table reflection

--- mysite/polls/session.py
+++ mysite/polls/session.py
@@ -2,6 +2,7 @@ import os
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.engine import reflection
+from typing import List
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -10,31 +11,48 @@ sqlite_path = os.path.join(BASE_DIR, 'db.sqlite3')
 sqlite_path_str = f"sqlite:///{sqlite_path}"
 engine = sqlalchemy.create_engine(sqlite_path_str)
 
+
+# Create a session for SQLAlchemy's ORM and querying the tables
+Session = sessionmaker(bind=engine)
+session = Session()
+
 # Automatically read the database tables and create metadata
 metadata = sqlalchemy.MetaData()
 metadata.reflect(bind=engine)
 insp = reflection.Inspector.from_engine(engine)
 
-# Create a session, to query the tables
-Session = sessionmaker(bind=engine)
-session = Session()
-
 
 # Create table reflection
 auth_user_table = metadata.tables['auth_user']
+polls_choice_table = metadata.tables['polls_choice']
+polls_question_table = metadata.tables['polls_question']
 
 
-def get_all_meta_tables():
+def get_all_tables() -> List:
+    """
+    `get_all_tables` helper function to get a list of all tables in the database
+
+    :return list all table names:
+    """
     return insp.get_table_names()
 
 
-def print_meta_columns(table):
+def print_table_meta_columns(table) -> None:
+    """
+    `print_table_meta_columns helper function to stdout to terminal the column names of a table
+
+    :param table:
+    :return None:
+    """
+
     print(table._columns)
 
 
-# Snippets
-print_meta_columns(auth_user_table)
-# print(get_all_meta_tables())
+# Code Snippets
+django_tables = get_all_tables()
+print(django_tables)
+
+print_table_meta_columns(auth_user_table)
 
 
 
