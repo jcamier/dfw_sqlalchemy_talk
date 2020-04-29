488893dd128f8e6408da1e98ac4a0c7df7cb402f
Jacques Camier <jcamier@gmail.com>
2020-04-28 22:13:07 -0500

    update views section

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,17 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
 
 from .models import Question
 from .session import auth_user_table, polls_choice_table, polls_question_table, session
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    latest_question_list = session.query(polls_question_table).order_by(polls_question_table.c.pub_date.desc()) \
+                               .all()[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = session.query(polls_question_table).filter(polls_question_table.c.id == question_id).first()
+    if not question:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
